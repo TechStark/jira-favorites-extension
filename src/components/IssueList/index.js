22d217a83bf5aa9f6c59e7ff3fe29e2666a500bd
Wilson Tian <wilson.tian@perkinelmer.com>
2021-12-03 20:35:42 +0800

    change list to table

--- src/components/IssueList/index.js
+++ src/components/IssueList/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Table } from 'antd';
 import { ClockCircleOutlined, StarOutlined, StarFilled } from '@ant-design/icons';
 import TimeAgo from 'react-timeago';
 import { createStarService } from '@/star';
 import styles from './style.less';
+
+const StarIcon = ({ starred, onClick }) => {
+  return (
+    <span style={{ marginRight: '6px' }}>
+      {starred ? (
+        <StarFilled className={styles.starIcon} style={{ color: 'orange' }} onClick={onClick} />
+      ) : (
+        <StarOutlined className={styles.starIcon} onClick={onClick} />
+      )}
+    </span>
+  );
+};
 
 class IssueList extends React.Component {
   constructor(props) {
@@ -26,70 +39,69 @@ class IssueList extends React.Component {
       return true;
     };
 
-    const StarIcon = ({ issueKey, starred }) => {
-      return (
-        <span style={{ marginRight: '6px' }}>
-          {starred ? (
-            <StarFilled
-              className={styles.starIcon}
-              style={{ color: 'orange' }}
-              onClick={() => {
-                removeStar(issueKey).then(() => {
-                  unstarredKeys.push(issueKey);
-                  this.setState({ unstarredKeys });
-                });
-              }}
-            />
-          ) : (
-            <StarOutlined
-              className={styles.starIcon}
-              onClick={() => {
-                const favorite = issues.filter((x) => x.key === issueKey)[0];
-                addStar(issueKey, favorite).then(() => {
-                  const newUnstarredKeys = unstarredKeys.filter((x) => x !== issueKey);
-                  this.setState({ unstarredKeys: newUnstarredKeys });
-                });
-              }}
-            />
-          )}
-        </span>
-      );
+    const handleStarIconClick = (key, starred) => {
+      if (starred) {
+        removeStar(key).then(() => {
+          unstarredKeys.push(key);
+          this.setState({ unstarredKeys });
+        });
+      } else {
+        const favorite = issues.filter((x) => x.key === key)[0];
+        addStar(key, favorite).then(() => {
+          const newUnstarredKeys = unstarredKeys.filter((x) => x !== key);
+          this.setState({ unstarredKeys: newUnstarredKeys });
+        });
+      }
     };
+
+    const columns = [
+      {
+        title: 'Starred Time',
+        align: 'left',
+        width: 150,
+        render: (text, record) => {
+          const { key } = record;
+          const starred = isStarred(key);
+          return (
+            <div>
+              <StarIcon starred={starred} onClick={() => handleStarIconClick(key, starred)} />
+              <TimeAgo date={record.time} />
+            </div>
+          );
+        },
+      },
+      {
+        title: 'Issue Key',
+        align: 'left',
+        width: 160,
+        render: (text, record) => {
+          const { key } = record;
+          return (
+            <a target="_blank" rel="noopener noreferrer" href={`${site}/browse/${key}`}>
+              {key}
+            </a>
+          );
+        },
+      },
+      {
+        title: 'Title',
+        dataIndex: 'title',
+      },
+    ];
+
+    const dataSource = issues.sort((a, b) => {
+      if (a.time - b.time > 0) {
+        return -1;
+      } else if (a.time - b.time < 0) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
 
     return (
       <div>
-        {issues
-          .sort((a, b) => {
-            if (a.time - b.time > 0) {
-              return -1;
-            } else if (a.time - b.time < 0) {
-              return 1;
-            } else {
-              return 0;
-            }
-          })
-          .map(({ key, title, time }) => {
-            const starred = isStarred(key);
-            return (
-              <div key={key} className={styles.row}>
-                <div>
-                  <StarIcon issueKey={key} starred={starred} />
-                  <a target="_blank" rel="noopener noreferrer" href={`${site}/browse/${key}`}>
-                    {key}
-                  </a>
-                  : <span>{title}</span>
-                </div>
-                <div style={{ paddingLeft: '20px' }}>
-                  <span>
-                    <ClockCircleOutlined style={{ marginRight: '4px', color: '#ccc' }} />
-                    <span title={new Date(time)}>
-                      starred <TimeAgo date={time} />
-                    </span>
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+        <Table size="small" columns={columns} dataSource={dataSource} pagination={false} />
       </div>
     );
   }
